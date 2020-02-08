066d696efb231b5d6e653775c59f1861f0960878
steve0079 <49959235+steve0079@users.noreply.github.com>
2020-02-08 13:56:40 -0800

    Removed initial route

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB()
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: "Welcome to ThirtyOne Labs App" }));
-
 //Define Routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
